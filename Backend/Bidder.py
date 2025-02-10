7b4fa4b5efa03fba9d71265d27a1018124b245c0
Keshav-byt <158580043+Keshav-byt@users.noreply.github.com>
2025-02-10 17:30:11 +0530

    Update Bidder.py

--- Backend/Bidder.py
+++ Backend/Bidder.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
 from BidRequest import BidRequest
+from typing import Union, Dict, Any
 
 class Bidder(ABC):
-   # Abstract base class for bidders. Any custom bidder must implement the get_bid_price method.
     
     @abstractmethod
-    def get_bid_price(self, bid_request: BidRequest) -> int:
-       #:return: The bid price as an integer.
-
+    def getBidPrice(self, bid_request: BidRequest) -> int:
+        
         pass
+    
+    def get_bid_price(self, bid_request: BidRequest) -> int:
+        
+        return self.getBidPrice(bid_request)
